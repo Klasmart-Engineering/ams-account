efd21646366e95efe47cc640b942276b195e59f7
Kritesh <kritesh@calmid.com>
2019-10-14 12:16:51 +0900

    fixed review comment

--- src/handlers/avatar_upload.go
+++ src/handlers/avatar_upload.go
@@ -2,10 +2,13 @@ package handlers
 
 import (
 	"context"
+	"crypto/sha256"
 	"encoding/hex"
 
 	"bitbucket.org/calmisland/go-server-requests/apierrors"
 	"bitbucket.org/calmisland/go-server-requests/apirequests"
+	"github.com/calmisland/go-errors"
+	"github.com/google/uuid"
 )
 
 type avatarUploadRequestBody struct {
@@ -15,19 +18,19 @@ type avatarUploadRequestBody struct {
 }
 
 type avatarUploadResponseBody struct {
-	UploadURL    string `json:"uploadUrl"`
-	UploadMethod string `json:"uploadMethod"`
+	AttachmentID  string            `json:"attachmentId"`
+	UploadURL     string            `json:"uploadUrl"`
+	UploadMethod  string            `json:"uploadMethod"`
+	UploadHeaders map[string]string `json:"uploadHeaders,omitempty"`
 }
 
 const (
 	contentLengthHeaderName = "Content-Length"
+	imageContentType        = "image/jpeg"
 
-	imageContentType = "image/jpeg"
-
-	attachmentMaxSize = 10 * 1024 * 1024 // 10 MB
-
-	sha256ByteLength = 256 / 8
-	sha256HexLength  = sha256ByteLength * 2
+	attachmentMaxSize = 1 * 1024 * 1024 // 1 MB
+	sha256ByteLength  = sha256.Size
+	sha256HexLength   = sha256ByteLength * 2
 )
 
 // HandleAvatarUpload handles avtar upload requests.
@@ -54,14 +57,25 @@ func HandleAvatarUpload(ctx context.Context, req *apirequests.Request, resp *api
 	}
 
 	// Validate the content hash
-	contentHash, err := hex.DecodeString(reqBody.ContentSHA256)
-	if err != nil || len(contentHash) != sha256ByteLength {
+	contentSHA256Str := reqBody.ContentSHA256
+	contentSHA256, err := hex.DecodeString(contentSHA256Str)
+	if err != nil || len(contentSHA256) != sha256ByteLength {
 		return resp.SetClientError(apierrors.ErrorInvalidParameters.WithField("contentSha256"))
 	}
 
+	attachmentUUID, err := uuid.NewRandom()
+	if err != nil {
+		return resp.SetServerError(errors.Wrap(err, "Failed to generate an UUID"))
+	}
+
+	// Get the signed upload URL
+	attachmentID := attachmentUUID.String()
+
 	response := avatarUploadResponseBody{
-		UploadURL:    "https://not.yet.badanamu.net/upload",
-		UploadMethod: "POST",
+		AttachmentID:  attachmentID,
+		UploadURL:     "https://not.yet.badanamu.net/upload",
+		UploadMethod:  "POST",
+		UploadHeaders: nil,
 	}
 	resp.SetBody(&response)
 	return nil
