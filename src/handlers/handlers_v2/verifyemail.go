3b14af528f773a89f0402d4bd8f4248e0d32278c
Steve Song <steve.song@calmid.com>
2020-12-07 10:48:00 +0900

    feat: v2 verify email api

---
+++ src/handlers/handlers_v2/verifyemail.go
@@ -0,0 +1,41 @@
+package handlers_v2
+
+import (
+	"context"
+
+	"bitbucket.org/calmisland/account-lambda-funcs/src/globals"
+	"bitbucket.org/calmisland/go-server-requests/apierrors"
+	"bitbucket.org/calmisland/go-server-requests/apirequests"
+)
+
+type verifyEmailReqBody struct {
+	Email string `json:"email"`
+}
+
+type verifyEmailRespBody struct {
+	Verified bool `json:"verified"`
+}
+
+// HandleSignUp handles sign-up requests.
+func HandleVerifyEmail(_ context.Context, req *apirequests.Request, resp *apirequests.Response) error {
+	// Parse the request body
+	var reqBody verifyEmailReqBody
+	err := req.UnmarshalBody(&reqBody)
+	if err != nil {
+		return resp.SetClientError(apierrors.ErrorBadRequestBody)
+	}
+
+	email := reqBody.Email
+
+	_, ok, err := globals.AccountDatabase.GetAccountIDFromEmail(email)
+	if err != nil {
+		return resp.SetServerError(err)
+	}
+
+	response := verifyEmailRespBody{
+		Verified: ok,
+	}
+
+	resp.SetBody(&response)
+	return nil
+}
